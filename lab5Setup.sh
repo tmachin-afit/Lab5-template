4deb938262ad9b6cfba118c91d7cc14a4e6b7571
TIM <timothy.i.machin@gmail.com>
2024-05-03 15:53:11 +0000

    switching to notebook heatmap

--- lab5Setup.sh
+++ lab5Setup.sh
@@ -1,10 +1,10 @@
 ## RUNNING ON ACE
 # ensure updated system
-apt update && apt upgrade -y
-snap refresh
-snap install --classic code
+sudo apt update && sudo apt upgrade -y
+# snap refresh
+# snap install --classic code
 # install pip (python package manager) if not installed already
-apt install -y python3-tk
+sudo apt install -y python3-tk
 python3 -m pip install --upgrade pip
 # install python requirements
 python3 -m pip install -r requirements.txt
